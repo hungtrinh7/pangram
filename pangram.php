30a25ba9f0e7f3feb3e1a5a5d27a8c22d64d0732
Hung Trinh <hungtrinh7@users.noreply.github.com>
2022-06-21 21:50:39 +0700

    Create pangram.php

---
+++ pangram.php
@@ -0,0 +1,12 @@
+<?php  
+function isPangrams($s) {
+    $s = trim(strtolower($s));
+    $letters = str_split('abcdefghijklmnopqrstuvwxyz');
+    foreach ($letters as $letter) {
+        if (!strstr($s, $letter)) {
+            return false;
+        }
+    }
+    
+    return true;
+}
